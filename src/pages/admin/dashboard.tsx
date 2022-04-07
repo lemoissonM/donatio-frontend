e542f62f5268befc1bf4d9eea6a1abf4bb095692
lemoissonM <murhulametre@gmail.com>
2022-04-07 21:38:11 +0200

    show default image

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -66,7 +66,7 @@ const DashboardPage: React.FC = () => {
                         className="w-[20px] h-[20px] rounded-full mr-4"
                       />
                     ) : (
-                      <UserCircleIcon width={100} className="text-primary-900 mx-auto" />
+                      <UserCircleIcon width={20} className="text-primary-900 mx-auto" />
                     )}
                     <p className="text-blue text-sm font-semibold">{`${user['users_firstName']} ${user['users_lastName']}`}</p>
                     <p className="text-secondary-900 font-semibold text-sm ml-auto">
